2d1c3b24305adf05396d45b99f8c69114aa34546
skylar4808 <hskylar1984@gmail.com>
2025-03-16 18:27:23 +0000

    Skylar's Project

---
+++ Extra.java
@@ -0,0 +1,18 @@
+public class Extra {
+    
+    //DESCRIPTION: ascii art of house
+    //PRECONDITIONS:
+    //POSTCONDITIONS:
+    public static void artHouse(){
+    System.out.println("        `'::.                     ");
+    System.out.println("    _________H ,%%&%,            ");
+    System.out.println( "   /\\     _   %&&%%&%           ");
+    System.out.println("  /  \\___/^\\___\\%&%%&&           ");
+    System.out.println("  |  | []   [] |%\\Y&%'           ");
+    System.out.println("  |  |   .-.   | ||              ");
+    System.out.println("~~@._|@@_|||_@@|~||~~~~~~~~~~~~~ ");
+    System.out.println("     `\"\"\") )\"\"\"`                 ");
+    
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,39 +1,83 @@
 /********************************************
-*	AUTHORS:	<your names>
-* COLLABORATORS: <name of peer, tutor, instructor, anyone else who helped>
-*	LAST MODIFIED:	<date of last change>
+*	AUTHORS:	Skylar Hollingsworth,
+* COLLABORATORS: 
+*	LAST MODIFIED:	3/13/2025 by Skylar
 ********************************************/
 
 /********************************************
-*	<TITLE OF PROGRAM>
+*	<Housing Affordability Calculator>
+* https://calmatters.org/explainers/california-housing-costs-explainer/
 *********************************************
 *	PROGRAM DESCRIPTION:
-*	<1-2 sentences describing overall program>
+*	<Assists you in determining if the house/apartment you would like to buy/rent is affordable for you>
 *********************************************
 *	ALGORITHM:
 *	<Pseudocode here>
 *********************************************
 *	STATIC METHODS:
-* <list of static methods and which teammate made each>
+* <artHouse, calcIntroduction, availableIncome, rentAffordable - Skylar>
 *********************************************/
 
 
 public class Main 
 {
-  /***** CONSTANT SECTION *****/
+/***** CONSTANT SECTION *****/
+  final static String PROJECT_NAME = "Housing Affordability Calculator";
+  //Fictional Person's Data:
+  static int incomeAshley = 5000;
+  static int rentAshley = 1500;
+  static int expensesAshley = 2500;
 
-  public static void main(String[] args)
-  {
-    /***** DECLARATION SECTION *****/
-
-    /***** INITIALIZATION SECTION *****/
-
-    /***** INTRO SECTION *****/
-
-    /***** PROCESSING SECTION *****/
-
-    /***** OUTPUT SECTION *****/
+    public static void main(String[] args)
+{
+/***** DECLARATION SECTION *****/
     
+/***** INITIALIZATION SECTION *****/
+
+/***** INTRO SECTION *****/
+
+/***** PROCESSING SECTION *****/
+
+/***** OUTPUT SECTION *****/
+    Main.calcIntroduction(PROJECT_NAME);
+    Main.availableIncome (incomeAshley, expensesAshley);
+    Main.rentAffordable(rentAshley, incomeAshley);
+
+}
+
+/***** STATIC METHODS *****/
+  //DESCRIPTION: Determines the income available for rent after deducting personal expenses
+  //PRECONDITIONS:
+  //POSTCONDITIONS:
+  public static void availableIncome (int incomeActual, int expensesActual)
+  {
+    System.out.printf("Your current monthly income is: $%,d.%n", incomeActual);
+    System.out.printf("Your monthly expenses are: $%,d.%n", expensesActual);
+    System.out.printf("You have $%,d available for rent expenses.%n%n", (incomeActual - expensesActual));
   }
-  /***** STATIC METHODS *****/
+
+  //DESCRIPTION: Determines if rent is affordable for personal income/budget
+  //PRECONDITIONS:
+  //POSTCONDITIONS:
+  public static void rentAffordable (int rentActual, int incomeActual)
+  {
+    System.out.println("Is the home you want to rent afforable for your monthly budget?");
+    System.out.println("Your rent is considered affordable if it comprises 30% or less of your income.");
+    System.out.printf("If the home you rent costs $%,d per month.%n", rentActual);
+    System.out.printf("Then your rent would comprise %.2f%% of your income.%n%n", (rentActual / (double) incomeActual * 100));
+  }
+
+  //DESCRIPTION: Introducing calculator
+  //PRECONDITIONS:
+  //POSTCONDITIONS:
+  public static void calcIntroduction(String projectName)
+    {
+      System.out.println("------------------------------------");
+      Extra.artHouse();
+      System.out.println("------------------------------------\n");
+      System.out.println("Hi, everyone!");
+      System.out.println("This is the " + projectName + "!");
+      System.out.println("With this calculator you will be able to check the afforability of the home you would like to rent!\n");
+    }
+
 }
